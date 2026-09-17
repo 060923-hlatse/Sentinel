1ab176b090303b5ca6a0fe135b1c03f7734ad909
Patience Phasha <paphajhb025@student.wethinkcode.co.za>
2026-09-17 09:52:38 +0200

    added a new test

--- src/test/java/co/wethinkcode/sentinel/SentinelApplicationTest.java
+++ src/test/java/co/wethinkcode/sentinel/SentinelApplicationTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+import okhttp3.FormBody;
+import okhttp3.Request;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -45,6 +47,43 @@ class SentinelApplicationTest {
                     );
 
             assertEquals(403, response.code());
+        });
+
+        app.stop();
+    }
+
+    @Test
+    void adminShouldAccessAlerts() {
+
+        Javalin app =
+                SentinelApplication.createApp();
+
+        JavalinTest.test(app, (server, client) -> {
+
+      var loginResponse =
+        client.request(
+                "/login",
+                new Request.Builder()
+                        .post(
+                                new FormBody.Builder()
+                                        .add("username", "admin_sarah")
+                                        .add("password", "admin123")
+                                        .build()
+                        )
+        );
+
+        assertEquals(200, loginResponse.code());
+
+        String sessionId = loginResponse.body().string();
+                var response = client.get(
+                            "/admin/alerts",
+                            request -> request.header(
+                                    "X-Session-Id",
+                                    sessionId
+                            )
+                    );
+
+            assertEquals(200, response.code());
         });
 
         app.stop();
